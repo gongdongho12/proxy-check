e9460f4a5f336ceb3390c487ffb17e63f8c38248
Teddy (Dongho) <gongdongho12@gmail.com>
2024-07-31 18:16:07 +0900

    chore: 명령 수정

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -143,8 +143,8 @@ router.get("/generate_proxy", async (req, res) => {
 					console.log(`availableProxyPort, ${r}`, proxy.port);
 					availableProxyPorts.add(proxy.port);
 					if (password) {
-						const deleteCommand = `echo "${password}" | sudo -S "docker rm mitmproxy${proxy.port}"`;
-						const runCommand = `echo "${password}" | sudo -S "docker run -it --cpus=2 -m 1024m --memory-reservation=1024m --restart always -p ${proxy.port}:${proxy.port} --name mitmproxy${proxy.port} -d mitmproxy/mitmproxy:latest mitmproxy --set 	block_global=false --set listen_port=${proxy.port} --mode upstream:http://${proxy.host}:${proxy.port}"`;
+						const deleteCommand = `sudo docker rm mitmproxy${proxy.port}`;
+						const runCommand = `sudo docker run -it --cpus=2 -m 1024m --memory-reservation=1024m --restart always -p ${proxy.port}:${proxy.port} --name mitmproxy${proxy.port} -d mitmproxy/mitmproxy:latest mitmproxy --set 	block_global=false --set listen_port=${proxy.port} --mode upstream:http://${proxy.host}:${proxy.port}`;
 						console.log("deleteCommand", deleteCommand);
 						console.log("runCommand", runCommand);
 						return shellExec(deleteCommand).then(() =>
