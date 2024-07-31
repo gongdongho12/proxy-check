afb309c632a3c75f6d139c763ffec6c67eb95347
Teddy (Dongho) <gongdongho12@gmail.com>
2024-07-31 16:47:22 +0900

    chore: 체크 프록시도커 실행 로직 추가

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 // @ts-ignore
 import proxy_check from "proxy-check";
+import shellExec from "shell-exec";
 import dns from "dns";
 const router = Router();
 
@@ -8,12 +9,18 @@ function getDnsServers() {
 	return dns
 		.getServers()
 		.filter(
-			(v) => !(v.startsWith("168.126.63.") || v.startsWith("192.168.1."))
+			(v) =>
+				!(
+					v.startsWith("168.126.63.") ||
+					v.startsWith("192.168.1.") ||
+					v.startsWith("8.8.8.8") ||
+					v.startsWith("192.168.30.")
+				)
 		);
 }
 
 router.get("/check_proxy", async (req, res) => {
-	const { ports = ["8080", "8081"] }: any = req.query;
+	const { ports = ["8080", "8081"], password = undefined }: any = req.query;
 
 	console.log("req.query", req.query);
 	console.log("ports", ports);
@@ -38,6 +45,12 @@ router.get("/check_proxy", async (req, res) => {
 				.then((r: boolean) => {
 					console.log(`availableProxyPort, ${r}`, proxy.port);
 					availableProxyPorts.add(proxy.port);
+					if (password) {
+						const command = `echo "${password}" | sudo -S "docker run -it --cpus=2 -m 1024m --memory-reservation=1024m --restart always -p ${proxy.port}:${proxy.port} --name mitmproxy${proxy.port} -d mitmproxy/mitmproxy:latest mitmproxy --set 	block_global=false --set listen_port=${proxy.port} --mode upstream:http://${proxy.host}:${proxy.port}"`;
+						return shellExec(command).then(() => {
+							console.log("mitmproxy run");
+						});
+					}
 				})
 				.catch((e: any) => {
 					console.error(`port: ${proxy.port}`, e);
